06d75008d6b683ad644c36c2e1713b0919d65e65
Shisei Hanai <ruimo.uno@gmail.com>
2014-09-10 17:51:53 +0900

    Use curl.

--- getItemRecommend01.sh
+++ getItemRecommend01.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-http --json POST http://localhost:9001/recommendByItem <getItemRecommend01.json
+#http --json POST http://localhost:9001/recommendByItem <getItemRecommend01.json
+curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST --data @getItemRecommend01.json http://localhost:9001/recommendByItem
