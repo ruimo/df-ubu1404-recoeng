15193903a32be8a078aa6f8003642441ba20efd7
Ruimo Uno <ruimo.uno@gmail.com>
2014-10-11 06:53:22 +0900

    Remove RUNNING_PID file

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
 wget https://www.ruimo.com/jenkins/view/recoeng/job/recoeng-snapshot/lastSuccessfulBuild/artifact/target/universal/recoeng-${RECOENG_VERSION}.zip
 unzip -q recoeng-${RECOENG_VERSION}.zip
+rm recoeng-${RECOENG_VERSION}/RUNNING_PID
 recoeng-${RECOENG_VERSION}/bin/recoeng -Dhttp.port=9001
